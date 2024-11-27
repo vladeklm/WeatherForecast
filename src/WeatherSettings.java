fc0204ea4c531e56f4db1499e96cca74bb8b764f
vladislav <vladek.lm@mail.ru>
2024-11-27 14:58:00 +0300

    delete apikey

--- src/WeatherSettings.java
+++ src/WeatherSettings.java
@@ -1,4 +1,4 @@
 public class WeatherSettings {
     public static String BASE_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
-    public static String API_KEY = "EMRZYD7BZQP4FA2VY2SLQ3ESE";
+    public static String API_KEY = "";
 }
